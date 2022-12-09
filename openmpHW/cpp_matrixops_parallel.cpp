fe688ddca21e3757d3bbf5ff3e74934d86f47723
Klaas Willems <klaaswillems80@hotmail.com>
2022-12-09 16:11:52 +0100

    added omp pragmas

--- openmpHW/cpp_matrixops_parallel.cpp
+++ openmpHW/cpp_matrixops_parallel.cpp
@@ -12,12 +12,16 @@ namespace ublas = boost::numeric::ublas;
 void fullTimesDiagonal(ublas::matrix<double> &left, ublas::matrix<double> &right, ublas::matrix<double> &result)
 {
     size_t N = result.size1();
-    for (size_t i = 0; i < N; ++i)
+    #pragma omp parallel shared(left, right, result, N) private(i, j, k) num_threads(numThreads)
     {
-        for (size_t j = 0; j < N; ++j)
-        {
-            result(i, j) = left(i, j) * right(j, j);
-        }
+        #pragma omp for
+        for (size_t i = 0; i < N; ++i)
+            {
+                for (size_t j = 0; j < N; ++j)
+                {
+                    result(i, j) = left(i, j) * right(j, j);
+                }
+            }
     }
 }
 
@@ -43,26 +47,30 @@ void fullTimesFullBlocked(ublas::matrix<double> &left, ublas::matrix<double> &ri
 {
     size_t const blocksize = 50;
     size_t N = result.size1();
-    for (size_t i = 0; i < N / blocksize; ++i)
+    #pragma omp parallel shared(left, right, result, N) private(i, j, k) num_threads(numThreads)
     {
-        for (size_t j = 0; j < N / blocksize; ++j)
+        #pragma omp for
+        for (size_t i = 0; i < N / blocksize; ++i)
         {
-            for (size_t i_block = 0; i_block < blocksize; ++i_block)
-            {
-                for (size_t j_block = 0; j_block < blocksize; ++j_block)
-                {
-                    result(i * blocksize + i_block, j * blocksize + j_block) = 0.0;
-                }
-            }
-            for (size_t k = 0; k < N / blocksize; ++k)
+            for (size_t j = 0; j < N / blocksize; ++j)
             {
                 for (size_t i_block = 0; i_block < blocksize; ++i_block)
                 {
                     for (size_t j_block = 0; j_block < blocksize; ++j_block)
                     {
-                        for (size_t k_block = 0; k_block < blocksize; ++k_block)
+                        result(i * blocksize + i_block, j * blocksize + j_block) = 0.0;
+                    }
+                }
+                for (size_t k = 0; k < N / blocksize; ++k)
+                {
+                    for (size_t i_block = 0; i_block < blocksize; ++i_block)
+                    {
+                        for (size_t j_block = 0; j_block < blocksize; ++j_block)
                         {
-                            result(i * blocksize + i_block, j * blocksize + j_block) += left(i * blocksize + i_block, k * blocksize + k_block) * right(k * blocksize + k_block, j * blocksize + j_block);
+                            for (size_t k_block = 0; k_block < blocksize; ++k_block)
+                            {
+                                result(i * blocksize + i_block, j * blocksize + j_block) += left(i * blocksize + i_block, k * blocksize + k_block) * right(k * blocksize + k_block, j * blocksize + j_block);
+                            }
                         }
                     }
                 }
@@ -74,15 +82,20 @@ void fullTimesFullBlocked(ublas::matrix<double> &left, ublas::matrix<double> &ri
 void triangularTimesFull(ublas::matrix<double> &left, ublas::matrix<double> &right, ublas::matrix<double> &result)
 {
     size_t N = result.size1();
-    for (size_t i = 0; i < N; ++i)
+    
+    #pragma omp parallel shared(left, right, result, N) private(i, j, k) num_threads(numThreads)
     {
-        for (size_t j = 0; j < N; ++j)
-        {
-            result(i, j) = 0.0;
-            for (size_t k = i; k < N; ++k)
+        #pragma omp for
+        for (size_t i = 0; i < N; ++i)
             {
-                result(i, j) += left(i, k) * right(k, j);
+                for (size_t j = 0; j < N; ++j)
+                {
+                    result(i, j) = 0.0;
+                    for (size_t k = i; k < N; ++k)
+                    {
+                        result(i, j) += left(i, k) * right(k, j);
+                    }
+                }
             }
-        }
     }
 }
